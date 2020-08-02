da261771814eb45256a787149449802f5cdedf53
dt98 <49128931+danielterek@users.noreply.github.com>
2020-08-03 00:04:09 +0200

    Add files via upload

---
+++ pierre.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pierre</title>
+    <link rel="stylesheet" type="text/css" href="design.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>          
+        <img src="assets/img/pierre.png" alt="" class="logo">
+                 <nav>
+                     <ul class="navstop">
+                         <li><a href="#">Products</a> </li>
+                         <li><a href="#">References</a></li>
+                         <li> <a href="#">Features</a></li>
+                         <li><a href="#">About</a></li>  
+                    </ul>  
+                 </nav>
+            <a class="buy" href="#">Where to buy?</a>  
+    </header>
+
+    <header class="secondnav">
+            <ul class="scndnav">
+                <li><a>Home</a></li>
+                <li><a>Ipsum</a></li>
+                <li><a>Features</a></li>
+                <li class="ctrl">Heating controll</li>  
+            </ul>
+    </header>
+
+    <span class="control">
+        <h1 style>Heating control</h1>
+    </span>
+
+    <img src="assets/img/fullpic.png" alt="" class="fullpicture">
+
+         <p class="rnd">  
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of using
+          Lorem Ipsum is that it has a more-or-less normal distribution of letters, as 
+          opposed to using 'Content here, content here', making it look like readable English.
+        </p>  
+
+    <div class="container">
+            <div class="centerpics">
+
+                <img src="assets/img/1.png" alt="" class="firstpic">
+                    <div class="firstrow">
+                        <h3>Lorem ipsum sit donor</h3>
+                            <p> 
+                            It is a long established fact that a reader will be distracted by the readable
+                            content of a page when looking at its layout. The point of using Lorem Ipsum
+                            is that it has a more-or-less normal distribution of letters, as opposed to
+                            using 'Content here, content here', making it look like readable English. 
+                            Many desktop publishing packages and web page editors now use Lorem Ipsum 
+                            as their default model text. l be distracted by the readable
+
+                            content of a page when looking at its layout. The point of using Lorem Ipsum
+                            is that it has a more-or-less normal distribution of letters, as opposed to
+                            using.
+                            </p>
+                    </div>
+
+                <div class="secondrow">
+                    <p>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a 
+                    piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard
+                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of 
+                    the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through
+                    the cites of the word in classical literature, discovered the undoubtable source.
+                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+                    </p>
+                </div>
+                <img src="assets/img/2.png" alt="" class="secondpic">
+                
+                    <img src="assets/img/3.png" alt="" class="thirdpic">
+                <div class="thirdrow">
+                    <p>
+                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" 
+                    (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on 
+                    the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
+                    "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+                    </p>
+                </div>
+            </div>
+
+        <div class="lowercenter">
+        
+        </div>
+
+    </div>
+</body>  
+</html>
