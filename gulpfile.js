d8f2cf5460721ec6c4d4ce50faaec1ef7fd4196f
danielterek98 <49128931+danielterek@users.noreply.github.com>
2020-08-13 10:36:55 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,39 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') 
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
 
 gulp.task('browserSync', function() {
   browserSync.init({
      server: {
        baseDir: 'app'
-     }
+     },
    })
  });
- 
-gulp.task('watch', function(){ 
-   gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
-  // Other watchers
+
+ gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') 
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', gulp.series('browserSync', 'sass'), function (){
+//gulp.task('watch', function(){ 
+ // gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
+ // Other watchers
+//});
+ 
+
+
+gulp.task('watch', gulp.series('browserSync', 'sass'), function (){
   gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-// Other watchers 
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+
+
+
